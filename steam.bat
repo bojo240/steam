dea324ac4d33d66683f25a9fbbf2188ba9390717
bojo240 <bojo240@gmail.com>
2019-02-11 01:23:57 +0100

    asd

---
+++ steam.bat
@@ -0,0 +1,13 @@
+node steam1
+node steammerger
+node steam2
+node steammerger
+node steam3
+node steammerger
+node steam4
+node steammerger
+node steam5
+node steammerger
+git add *
+git commit -m "asd"
+git push
